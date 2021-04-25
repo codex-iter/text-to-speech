0bd21d17a473e13346d336f69b23e2d799542a7c
SumanMore <sumanmore322@gmail.com>
2021-04-25 17:26:18 +0530

    made changes in pg3.css project.html and script.js

--- script.js
+++ script.js
@@ -1,127 +1,56 @@
-const maxCharacters = 1000;
-const url = 'http://api.voicerss.org/';
-const apiKey = 'a2257984416a454da117006e5db0e280';
-var speed = 0;
-var lang = "en-in";
-
-const tooLongError = 'Text exceeds 1000 character maximum.';
-const whitespaceError ='Warning:"Text must contain text characters other than empty spaces."';
-
-const buildUrl = (str) => `${url}?key=${apiKey}&src=${str}&f=48khz_16bit_stereo&r=${speed}&hl=${lang}`;
-const tooLong = (str) => str.length >= maxCharacters;
-const emptyString = (str) => str.split(' ').every(_char => _char === '' || _char === '\n');
-
-const langBtnhi = () => document.getElementById('lang-btn-hi');
-const playBtn = () => document.getElementById('play-btn');
-const playBtn5x = () => document.getElementById('play-btn-5x');
-const playBtnm5x = () => document.getElementById('play-btn-m5x');
-const langBtnfr = () => document.getElementById('lang-btn-fr');
-const textInput = () => document.getElementById('text-input');
-const appContainer = () => document.getElementById('app-container');
-const errorContainer = () => document.getElementById('error-message');
-const clearErrors = () => errorContainer().innerHTML = '';
-
-const listenerFn = ($event) => {
-    if ($event.target.value || $event.type === 'paste'){
-        playBtn().disabled = false;
-        playBtn5x().disabled = false;
-        playBtnm5x().disabled = false;
-        langBtnhi().disabled = false;
-        langBtnfr().disabled = false;
-    }
-    else{
-        playBtn().disabled = true;
-        playBtn5x().disabled = true;
-        playBtnm5x().disabled = true;
-        langBtnhi().disabled = true;
-        langBtnfr().disabled = true;
-    }
-};
-
-const displayErrorMsg = (val) => {
-    var errs = new Array();
-    if (tooLong(val)) errs.push(tooLongError);
-    if (emptyString(val)) errs.push(whitespaceError);
-    errs.forEach(_err => {
-        const div = document.createElement('div');
-        div.innerText = _err;
-        errorContainer().appendChild(div);
-    });
-};
+txtinput = document.getElementById('txtinput');
+rateI = document.getElementById('rateI');
+InputRate = document.getElementById('InputRate');
+pitchI = document.getElementById('pitchI');
+InputPitch = document.getElementById('InputPitch');
+InputVoice = document.getElementById('InputVoice');
+speakBtn = document.getElementById('speakBtn');
 
 
+InputRate.addEventListener('change',function(){
+    rateI.textContent = InputRate.value
+})
 
-playBtn().addEventListener('click', () => {
-    clearErrors();
 
-    if (!emptyString(textInput().value) && !tooLong(textInput().value)) {
-        textInput().value = textInput().value.trim();
-        new Audio(buildUrl(textInput().value)).play();
-        
-    }
-    else
-        displayErrorMsg(textInput().value);
+InputPitch.addEventListener('change',function(){
+    pitchI.textContent = InputPitch.value
+})
 
-});
-playBtn5x().addEventListener('click', () => {
-    clearErrors();
+ synth = window.speechSynthesis;
 
-    if (!emptyString(textInput().value) && !tooLong(textInput().value)) {
-        speed=5;
-        textInput().value = textInput().value.trim();
-        new Audio(buildUrl(textInput().value)).play();
-        
-    }
-    else
-        displayErrorMsg(textInput().value);
 
-});
-playBtnm5x().addEventListener('click', () => {
-    clearErrors();
+ getVoices = () => {
+    
+  allVoices = synth.getVoices();
+  allVoices.forEach(voice => {
+    option  = document.createElement('option')
+    option.textContent = voice.name;
+    option.setAttribute('data-voice',voice.name);
+    InputVoice.appendChild(option)
+  }) 
+} 
 
-    if (!emptyString(textInput().value) && !tooLong(textInput().value)) {
-        speed=-5;
-        textInput().value = textInput().value.trim();
-        new Audio(buildUrl(textInput().value)).play();
-        
-    }
-    else
-        displayErrorMsg(textInput().value);
+speechSynthesis.onvoiceschanged  = getVoices 
+getVoices()
 
-});
-langBtnhi().addEventListener('click', () => {
-    clearErrors();
+function speakIT(){
+if(txtinput.value!=''){
+sayThis = new SpeechSynthesisUtterance(txtinput.value); 
+ selectedVoice = InputVoice.selectedOptions[0].getAttribute('data-voice');
 
-    if (!emptyString(textInput().value) && !tooLong(textInput().value)) {
-        lang = "hi-in";
-        textInput().value = textInput().value.trim();
-        new Audio(buildUrl(textInput().value)).play();
-        
-    }
-    else
-        displayErrorMsg(textInput().value);
-
-});
-langBtnfr().addEventListener('click', () => {
-    clearErrors();
-
-    if (!emptyString(textInput().value) && !tooLong(textInput().value)) {
-        lang = "fr-fr";
-        textInput().value = textInput().value.trim();
-        new Audio(buildUrl(textInput().value)).play();
-        
-    }
-    else
-        displayErrorMsg(textInput().value);
-
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const containerHeight = appContainer().clientHeight;
-    const docHeight = window.innerHeight;
-    appContainer().style.marginTop = `${docHeight/2 - containerHeight/2 - 25}px`;
-    textInput().addEventListener('keyup', listenerFn);
-    textInput().addEventListener('paste', listenerFn);
-
+ allVoices.forEach(voice => {
+if(selectedVoice == voice.name){
+    sayThis.voice = voice;
 }
-);
+}) 
+
+sayThis.rate = InputRate.value
+sayThis.pitch = InputPitch.value
+synth.speak(sayThis)
+} 
+}
+speakBtn.addEventListener('click',function(){
+    speakIT()
+})
+
+
